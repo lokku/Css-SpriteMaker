3e169b002f3ca333ec0ed345da3433f844818866
Savio Dimatteo <darksmo@gmail.com>
2014-05-14 01:03:25 +0200

    Bump up version number

--- lib/CSS/SpriteMaker.pm
+++ lib/CSS/SpriteMaker.pm
@@ -22,11 +22,11 @@ CSS::SpriteMaker - Combine several images into a single CSS sprite
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 
 =head1 SYNOPSIS
